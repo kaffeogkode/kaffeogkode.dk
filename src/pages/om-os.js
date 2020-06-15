ab3104f6efc54ad67417f4aa3fd9229225d7d97c
Isabella Karpf <swungify@users.noreply.github.com>
2020-06-15 23:32:29 +0200

    Mere om os ✍️og projektet

--- src/pages/om-os.js
+++ src/pages/om-os.js
@@ -7,12 +7,25 @@ import SEO from "../components/seo"
 const OmOsPage = () => (
   <Layout>
     <SEO title="Om os" />
+    <h1>Idéen bag</h1>
+    <p>I 2018 blev frøet sået til Kaffe & Kode efter at have arbejdet sammen i ABC Softwork. Vi sad selv med kaffe og kode, 
+      og det blev rutinen at mødes i kaffebaren.
+      Tiden gik og projektet omkring et podcast blev overskygget af andre ting indtil, at COVID-19 satte verden over styr. 
+      På mange punkter har Kaffe og Kode fået nyt liv af lockdownen med øget fokus på <i>coronaprojekter</i>.
+      Idéen som startede som et podcast blev til en livestream på Twitch. Vores koncept er nu at livestreame vores projekter, 
+      som derefter vil danne indhold for vores podcasts. 
+    </p>
+    <p>Så kom og ta' en kaffe med os! ☕️</p>
+
+    <br></br>
     <h1>Om os</h1>
     <h2>Isabella</h2>
-    <p>Isabella er uddannet softwareudvikler fra ITU og bor i Frankrig.</p>
+    <p>Isabella har en kandidat i softwareudvikling med speciale indenfor kunstig intelligens og machine learning.
+       Isabellas yndlingskaffe er en <i>smooth</i> cappuccino.
+    </p>
     <p><a href="https://www.isabellakarpf.com/">https://www.isabellakarpf.com/</a></p>
     <h2>Michael</h2>
-    <p>Michael er uddannet ingeniør og CTO i <a href="https://abcsoftwork.com/">ABC Softwork</a>.</p>
+    <p>Michael er uddannet ingeniør og CTO i <a href="https://abcsoftwork.com/">ABC Softwork</a>. Michaels yndlingskaffe er en dobbelt café latte.</p>
     <p><a href="https://michaelbanzon.com/">https://michaelbanzon.com/</a></p>
   </Layout>
 )
