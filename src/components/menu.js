ed652308b955689b987fd76ae9d848f04d5460a3
Michael Banzon <michael@banzon.dk>
2020-06-21 21:36:07 +0200

    Fixes #1
    Nu med ikoner 🤩🥳🎉💩

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,18 +1,24 @@
 import React from "react"
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab, faYoutube, faTwitch, faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faHome, faClipboardList, faPoo } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './menu.css'
 import { Link } from "gatsby"
+
+library.add(fab, faYoutube, faTwitch, faTwitter, faGithub, faHome, faClipboardList, faPoo )
 
 const Menu = () => {
   return (
     <>
         <nav className="menu">
-            <Link to="/">Forside</Link>
-            <Link to="/projekter">Projekter</Link>
-            <Link to="/om-os">Om os</Link>
-            <a href="https://twitch.tv/kaffeogkode">Twitch</a>
-            <a href="https://twitter.com/kaffeogkode">Twitter</a>
-            <a href="https://youtube.com/">YouTube</a>
-            <a href="https://github.com/kaffeogkode">GitHub</a>
+            <Link to="/" title="Forside"><FontAwesomeIcon icon="home" /></Link>
+            <Link to="/projekter" title="Projekter"><FontAwesomeIcon icon="clipboard-list" /></Link>
+            <Link to="/om-os" title="Om Kaffe &amp; Kode"><FontAwesomeIcon icon="poo" /></Link>
+            <a href="https://twitch.tv/kaffeogkode" title="Vores Twitch-kanal"><FontAwesomeIcon icon={["fab", "twitch"]} /></a>
+            <a href="https://twitter.com/kaffeogkode" title="Vores Twitter"><FontAwesomeIcon icon={["fab", "twitter"]} /></a>
+            <a href="https://www.youtube.com/channel/UCnvLGoN3BB5OZdY0XW6IMtw" title="Vores YouTube-kanal"><FontAwesomeIcon icon={["fab", "youtube"]} /></a>
+            <a href="https://github.com/kaffeogkode" title="Vores GitHub"><FontAwesomeIcon icon={["fab", "github"]} /></a>
         </nav>
     </>
   )
