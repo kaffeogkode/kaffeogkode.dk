dd51a2302974055879c17caabd106ef9a4a80090
Michael Banzon <michael@banzon.dk>
2020-12-06 20:54:32 +0100

    Update projektpreview.js
    Retter specialtegn i excerpt 😵

--- src/components/projektpreview.js
+++ src/components/projektpreview.js
@@ -4,7 +4,7 @@ import React from 'react'
 const ProjectPreview = ({p}) => (
     <>
         <h2>{p.title}</h2>
-        <p>{p.excerpt}</p>
+        <p dangerouslySetInnerHTML={{ __html: p.excerpt}}></p>
         <Link to={`/projekt/${p.slug}`}>Læs mere</Link>
         <hr />
     </>
